fc9b508261f2beb2729e3e2d02d60244c0e74811
KarasiK <polkavod@gmail.com>
2022-12-07 11:55:07 +0700

    Add UI

---
+++ app/src/main/java/ru/avtf/kitpo/kitpo_rgr_konovalov/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.avtf.kitpo.kitpo_rgr_konovalov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
